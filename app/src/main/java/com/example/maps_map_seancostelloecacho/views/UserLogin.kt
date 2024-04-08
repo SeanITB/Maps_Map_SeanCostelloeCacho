8bb91588f1260ded69a771713b6c9d6f90f445e9
SeanITB <sean.costelloe.7e5@itb.cat>
2024-04-08 16:03:35 +0100

    start user register

--- app/src/main/java/com/example/maps_map_seancostelloecacho/views/UserLogin.kt
+++ app/src/main/java/com/example/maps_map_seancostelloecacho/views/UserLogin.kt
@@ -1,9 +1,15 @@
 package com.example.maps_map_seancostelloecacho.views
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -11,45 +17,88 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.example.maps_map_seancostelloecacho.viewModel.MarkerViewModel
+import javax.annotation.Untainted
+
 
 @Composable
-fun UserLoginContent() {
-    
+fun UserLoginContent(navController: NavController, markerVM: MarkerViewModel) {
+    val userName by markerVM.userName.observeAsState("")
+    val password by markerVM.password.observeAsState("")
+    val goToNext by markerVM.goToNext.observeAsState(false)
+    val navigationItems by markerVM.navigationItems.observeAsState(mapOf())
+    val isLoading by markerVM.isLoading.observeAsState(true)
+
+    UserLoginView(userName = userName, password = , onUserNameChange = )
+
+
 }
-/*
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun UserLoginView() {
-    val arrVisibilityLable = arrayOf("Password Visibility ", "Repeat Password Visibility")
-    TextField(
-        value = arrPasswords[index],
-        onValueChange = { arrChangePasswordFunctions[index](it) },
-        placeholder = { Text(text = arrPasswordLable[index]) },
-        colors = TextFieldDefaults.outlinedTextFieldColors(
-            focusedBorderColor = MaterialTheme.colorScheme.primary,
-            unfocusedBorderColor = MaterialTheme.colorScheme.background
-        ),
-        maxLines = 1,
-        singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-        trailingIcon = {
-            val image = if (passwordVisibilty) {
-                Icons.Filled.VisibilityOff
+fun UserLoginView(
+    userName: String,
+    password: String,
+    onUserNameChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+) {
+    var visability by rememberSaveable {
+        mutableStateOf(false)
+    }
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = userName,
+            onValueChange = { onUserNameChange(it) },
+            placeholder = { Text(text = "email") },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = MaterialTheme.colorScheme.primary,
+                unfocusedBorderColor = MaterialTheme.colorScheme.background
+            ),
+            singleLine = true,
+        )
+        TextField(
+            value = password,
+            onValueChange = { onPasswordChange(it) },
+            placeholder = { Text(text = "Password" )},
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = MaterialTheme.colorScheme.primary,
+                unfocusedBorderColor = MaterialTheme.colorScheme.background
+            ),
+            maxLines = 1,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                val image = if (visability) {
+                    Icons.Filled.VisibilityOff
+                } else {
+                    Icons.Filled.Visibility
+                }
+                IconButton(onClick = { visability = !visability }) { //toDo: chacer que solo se muestre uno quando canvias la visibilidad
+                    Icon(imageVector = image, contentDescription = "Visibility password")
+                }
+            },
+            visualTransformation = if (visability) {
+                VisualTransformation.None
             } else {
-                Icons.Filled.Visibility
+                PasswordVisualTransformation()
             }
-            IconButton(onClick = { arrChangeVisibilityFunctions[index](!arrVisibilities[index]) }) { //toDo: chacer que solo se muestre uno quando canvias la visibilidad
-                Icon(imageVector = image, contentDescription = arrVisibilityLable[index])
-            }
-        },
-        visualTransformation = if (passwordVisibilty) {
-            VisualTransformation.None
-        } else {
-            PasswordVisualTransformation()
-        }
-    )
+        )
+    }
 }
 
- */
